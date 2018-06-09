3dae386aabd3426fd0608dbc0974a96004f972ff
ali <alir.raza@mail.utoronto.ca>
2018-06-08 20:27:43 -0400

    app1.py and search_results need fixing

---
+++ app1.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+#from webScraper import HTMLParser
+#from prerequisite import recognized_prereq
+#web = __import__('webScraper')
+
+#rec_prereq = __import__('prerequisite')
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.test
+collection = db.courses
+prereq = db.prereq
+
+@app.route('/')
+def INDEX():
+    return render_template("index.html")
+
+@app.route('/search', methods=["POST", "GET"])
+def search_post():
+    data = request.args.get('course').upper()
+    info=""
+    
+    courseData = collection.find().batch_size(300)#.find_one({'course code':data})
+    for i in courseData:
+        if i.get('course code')==data:
+            info = i
+            break;
+        if i.get('course code')==data+"H5":
+            info = i
+            data = data+"H5"
+            break;
+        if i.get('course code')==data+"Y5":
+            info = i
+            data = data+"Y5"
+            break;
+
+
+    Prereq = prereq.find_one({data: {'$exists' : True}})
+    if info:
+        if Prereq:
+            return render_template("search_results.html",Data = info,prereqs = Prereq[data])
+        else:
+            return render_template("search_results.html",Data = info)
+    else:
+        return render_template("/index.html")
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
--- testing2.py
+++ testing2.py
@@ -22,19 +22,31 @@ class main():
         self.course_unique_utm = {}
         self.courses_unique_utsg = {}
         self.courses_unique_utsc = {}
-        utm_courses.drop()
+
         self.utm_courses()
+        self.utsc_courses()
+        self.utsg_courses()
 
     def utm_courses(self):
+        utm_courses.drop()
         courses=[]
-        for i in cobalt_courses.find({"campus": "UTM"}).distinct("code"):
-            if(len(i)==9):
-                courses.append(i)
+        for i in cobalt_courses.find({"campus": "UTM"}).distinct("name"):
+            courses.append(i)
         for i in courses:
-            dic = cobalt_courses.find({"campus": "UTM", "code":i})
+            a = False
+            dic = cobalt_courses.find({"campus": "UTM", "name":i})
+            # utm_courses.insert(dic[0])
+            for j in dic:
+                if len(j["code"])==9:
+                    a = True
+                    utm_courses.insert(j)
+                    break
 
-            utm_courses.insert(dic[0])
+            if a==False:
+                b=cobalt_courses.find({"campus": "UTM", "name":i})
+                utm_courses.insert(b[0])
 
+        print("utm done")
         # j = cobalt_courses.find({"code":courses[0], "campus": "UTM","term":'2019 Winter'})
         # pprint.pprint(j[0])
 
@@ -42,8 +54,40 @@ class main():
         # for i in courses:
         #     cobalt_courses.find({"campus": "UTM", "name")
 
-
-
+    def utsg_courses(self):
+        utsg_courses.drop()
+        courses=[]
+        for i in cobalt_courses.find({"campus": "UTSG"}).distinct("name"):
+            courses.append(i)
+        for i in courses:
+            a=False
+            dic = cobalt_courses.find({"campus": "UTSG", "name":i})
+            for j in dic:
+                if len(j["code"])==9:
+                    utsg_courses.insert(j)
+                    a = True
+                    break
+            if not a:
+                b=cobalt_courses.find({"campus": "UTSG", "name":i})
+                utsg_courses.insert(b[0])
+        print("utsg done")
+    def utsc_courses(self):
+        utsc_courses.drop()
+        courses=[]
+        for i in cobalt_courses.find({"campus": "UTSC"}).distinct("name"):
+            courses.append(i)
+        for i in courses:
+            a=False
+            dic = cobalt_courses.find({"campus": "UTSC", "name":i})
+            for j in dic:
+                if len(j["code"])==9:
+                    utsc_courses.insert(j)
+                    a = True
+                    break
+            if not a:
+                b=cobalt_courses.find({"campus": "UTSC", "name":i})
+                utsc_courses.insert(b[0])
+        print("utsc done")
 
 
 if __name__ == '__main__':
